b2a897bee214625fcda0b13763170816d0fe832a
Vivek09Chahal <vivekchahal1156@gmail.com>
2024-03-13 11:07:13 +0530

    did some change in file usingKeyword file

--- advance cpp/usingKeyword.cpp
+++ advance cpp/usingKeyword.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Base{
     public: int a ;
-    Base(int a): a(a){cout << "Base Constructor" << endl;};
+    Base(int a): a(a){cout << "Base Constructor" << endl;}
 };
 
 class Derived : public Base{
